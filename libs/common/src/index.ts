18fdedba34065ab6c716f5453422296c8656d028
Andrei-David Nan <nandavid190@gmail.com>
2024-03-01 18:15:16 +0200

    v0.9 - setup billing microservice, Dockerfile, update docker-compose;

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,5 @@
 export * from './db/database.module';
 export * from './db/abstract.repository';
 export * from './db/abstract.schema';
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
