d2a48cf89303a1d8330ea02bb239c41515c0d842
Andrei-David Nan <nandavid190@gmail.com>
2024-03-09 15:37:37 +0200

    v0.11 - setup event handler for orders event in billing;

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { BillingService } from './billing.service';
+import { RmqService } from '@app/common';
+import { EventPattern, Payload, Ctx } from '@nestjs/microservices/decorators';
+import { RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.billingService.getHello();
+  @EventPattern('order_created')
+  async handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.billingService.bill(data);
+    this.rmqService.ack(context);
   }
 }
