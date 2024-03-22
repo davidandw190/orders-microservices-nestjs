f7e9608a59217d042363de5749ac356c02bc8153
Andrei-David Nan <nandavid190@gmail.com>
2024-03-22 11:58:39 +0200

    v0.16 - bootstrap auth micro, config Dockerfile auth micro;

--- libs/common/src/db/abstract.repository.ts
+++ libs/common/src/db/abstract.repository.ts
@@ -17,10 +17,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     private readonly connection: Connection,
   ) {}
 
-  async create(
-    document: Omit<TDocument, '_id'>,
-    options?: SaveOptions,
-  ): Promise<TDocument> {
+  async create( document: Omit<TDocument, '_id'>, options?: SaveOptions): Promise<TDocument> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
