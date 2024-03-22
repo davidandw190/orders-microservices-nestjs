2223e37736ca1f3166fc1e29fb58beec7a84ee1b
Andrei-David Nan <nandavid190@gmail.com>
2024-03-22 19:07:32 +0200

    v0.18 - setup jwt strategy, auth service and auth controller;

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { JwtAuthGuard } from '@app/common/auth/jwt.auth.guard';
+import { User } from './users/schemas/user.schema';
+import { Response } from 'express';
 
-@Controller()
+
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
-  }
+ @Post('login')
+ async login(user: User, @Res({ passthrough: true })response: Response) {
+  await this.authService.login(user, response);
+  response.send(user);
+ }
+
+ @UseGuards(JwtAuthGuard)
+ @MessagePattern('validate_user')
+ async validateUser(user: User) {
+   return user;
+ }
 }
--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import * as Joi from 'joi';
+import { DatabaseModule, RmqModule } from '@app/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    RmqModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/auth/.env'
+    }),
+    JwtModule.registerAsync({
+      
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_SECRET')}s`,
+
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
