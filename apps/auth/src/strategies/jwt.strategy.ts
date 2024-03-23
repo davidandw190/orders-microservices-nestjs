11f06ad0550665c83b16d97bf9b1ad34ecb0f138
Andrei-David Nan <nandavid190@gmail.com>
2024-03-23 22:57:34 +0200

    v0.23 -  implement CanActivate via JwtAuthGuard in libs/common/auth;

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,33 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { TokenPayload } from '../auth.service';
-import { ExtractJwt } from 'passport-jwt';
-import { Types } from 'mongoose';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { catchError, Observable, tap } from 'rxjs';
+import { AUTH_SERVICE } from './services';
 
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    configService: ConfigService,
-    private readonly usersService: UsersService,
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: any) => {
-          return request?.Authentication;
-        },
-      ]),
-      secretOrKey: configService.get('JWT_SECRET'),
-    });
-  }
 
-	async validate({ userId }: TokenPayload) {
-    try {
-      return await this.usersService.getUser({
-        _id: new Types.ObjectId(userId),
-      });
-    } catch (err) {
-      throw new UnauthorizedException();
-    }
-  }
-}
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -3,3 +3,5 @@ export * from './db/abstract.repository';
 export * from './db/abstract.schema';
 export * from './rmq/rmq.service';
 export * from './rmq/rmq.module';
+export * from './auth/auth.module';
+export * from './auth/jwt.auth.guard';
