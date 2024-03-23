b477c82f338c4d9b649bd28e114f1bf588d71de9
Andrei-David Nan <nandavid190@gmail.com>
2024-03-23 22:14:01 +0200

    v0.22 -  small fixes;

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -4,15 +4,16 @@ import { MessagePattern } from '@nestjs/microservices';
 import { JwtAuthGuard } from '@app/common/auth/jwt.auth.guard';
 import { User } from './users/schemas/user.schema';
 import { Response } from 'express';
+import { CurrentUser } from './current-user.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 
-@UseGuards(LocalAuthGuard)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
  @Post('login')
+ @UseGuards(LocalAuthGuard)
  async login(user: User, @Res({ passthrough: true })response: Response) {
   await this.authService.login(user, response);
   response.send(user);
@@ -20,7 +21,7 @@ export class AuthController {
 
  @UseGuards(JwtAuthGuard)
  @MessagePattern('validate_user')
- async validateUser(user: User) {
+ async validateUser(@CurrentUser() user: User) {
    return user;
  }
 }
